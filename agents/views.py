36dd1c7fbe109c7a403f16b0ae5776acc1553f3a
KAVYANGANA <kavyangana89@gmail.com>
2026-04-29 18:03:54 +0530

    Implement agent creation flow with user account integration, and validation

--- agents/views.py
+++ agents/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from .models import Agent
 from .serializers import AgentSerializer
 
@@ -8,3 +8,7 @@ from .serializers import AgentSerializer
 class AgentViewSet(ModelViewSet):
     queryset = Agent.objects.all()
     serializer_class = AgentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
