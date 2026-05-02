46b1aec2be002c5e9671c49613962d30a59fac50
shahana muneer <shahanamuneer26@gmail.com>
2026-05-02 10:30:10 +0400

    Remove venv from Git tracking

--- manage.py
+++ manage.py
@@ -6,7 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
+<<<<<<< HEAD
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'back_end.settings')
+=======
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CRMpro.settings')
+>>>>>>> dfa4c08 (Remove venv from Git tracking)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
